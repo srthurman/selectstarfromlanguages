72f0452d4c8d77396b6b635f3d54068aa960ac81
Sara Thurman <srthurman@gmail.com>
2015-03-05 11:35:04 -0500

    update CNAME to blog.selectstarfromlanguages.com

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Sara Thurman'
 SITENAME = u'Select * from Languages'
+#SITEURL = ''
 SITEURL = ''
 
 PATH = 'content'
@@ -11,6 +12,15 @@ PATH = 'content'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
+
+
+# Static paths will be copied without parsing their contents
+STATIC_PATHS = ['extra']
+
+# Shift the installed location of a file
+EXTRA_PATH_METADATA = {
+        'extra/CNAME': {'path': 'CNAME'},
+        }
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -27,15 +37,16 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 
 # Social widget
 SOCIAL = (('Github', 'https://github.com/srthurman'),
-          ('Another social link', '#'),)
+          ('Twitter', 'https://twitter.com/thurmSR'),)
 
 DEFAULT_PAGINATION = 10
 
 # Theme settings
-THEME = 'gum'
+THEME = '/home/sarat/python/pelican_themes/pelican-themes/gum'
 
 
 GITHUB_URL = 'https://github.com/srthurman'
+TWITTER_URL = 'https://twitter.com/thurmSR'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 #SITEURL = 'http://selectstarfromlanguages.com'
-SITEURL = ''
+SITEURL = 'http://www.selectstarfromlanguages.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
